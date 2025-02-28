77da1dfa13d20df8d0c6ef4ea8e686b48696872b
cents97 <j.centsinno>
2025-02-28 17:38:07 +0300

    change3

--- IT_APP/urls.py
+++ IT_APP/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('team/', views.team, name='team'),
     path('project/', views.project, name='project'),
-     path('product/<int:product_id>/request/', views.request_product, name='request_product'),
-       path('products/', views.all_products, name='all_products'),  # View all products
-        path("save-message/", views.save_contact_message, name="save_message"),
+    path('product/<int:product_id>/request/', views.request_product, name='request_product'),
+    path('products/', views.all_products, name='all_products'),  # View all products
+    path("save-message/", views.save_contact_message, name="save_message"),
 ]
